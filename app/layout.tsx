9d7a0ef37b2334d9f1a092faa1aae3f5a5b2e738
yogyy <yogimr261@gmail.com>
2024-02-15 21:32:02 +0700

    feat: add Sessions, Cookies, JWTs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,19 @@ export const metadata = {
   title: "Next.js Authentication",
   description: "Example using NextAuth.js",
 };
-
-export default function RootLayout({
-  children,
-}: {
+interface LayoutProps {
   children: React.ReactNode;
-}) {
+}
+export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body
+        style={{
+          backgroundColor: "#0c0c0c",
+          color: "#f0f0f0",
+        }}>
+        {children}
+      </body>
     </html>
   );
 }
